1b7e50951e5efb7a2422fcc1f5487dbe1f7b6eac
Siddhant saxena <66401356+mr-siddy@users.noreply.github.com>
2020-08-25 19:31:26 +0530

    Day-1
     Image Processing with NumPy and OpenCV

---
+++ Day-1/faces.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 16:31:15 2020
+
+@author: mrsid
+"""
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while True:
+     
+    ret, frame = cap.read()
+    
+    if ret:
+        faces = classifier.detectMultiScale(frame)
+        
+        for face in faces:
+            x,y,w,h = face
+            frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 4)
+            
+            
+        cv2.imshow("My window", frame)
+        
+    key = cv2.waitKey(30)
+    
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
